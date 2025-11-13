30cb2de836f6d29fa1ffbc4b702071dd3b23b7fa
aubert-co <blacket021@gmail.com>
2025-11-13 00:49:34 -0300

    chore: add helmet, cors configs, routes, rate-limit

--- app/service/src/server.ts
+++ app/service/src/server.ts
@@ -1,5 +1,9 @@
 import express from 'express'
 import rateLimit from 'express-rate-limit';
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import helmet from 'helmet'
+import route from './route/auth.route';
 
 const globalLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, 
@@ -13,4 +17,36 @@ const globalLimiter = rateLimit({
 });
 const app = express()
 
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      useDefaults: true,
+    },
+  })
+);
+if(process.env.MODE !=="test"){
+  app.set('trust proxy',1)
+}
+app.use(cors({
+  methods:['POST'],
+  credentials:true,
+  origin:"https://market.aubertdev.com.br"
+}))
+app.use( cookieParser())
+app.use( globalLimiter )
 
+app.use(route)
+const startServer = async()=>{
+    try {
+      
+        if(process.env.MODE !== "test"){
+            app.listen(process.env.PORT,()=>{console.log('server running'+process.env.PORT)});
+        }
+    } catch (err:any) {
+        console.error('Erro ao iniciar servidor:', err);
+        process.exit(1);
+    }
+
+}
+startServer() 
+export default app;
